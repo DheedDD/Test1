04359867c8e5f6267a20195e8676c75b3e7b91c1
Dheed <admin1@astra>
2023-04-02 12:37:30 +0400

    IRon

---
+++ src/Calculator/Caculator.java
@@ -0,0 +1,19 @@
+package ru.gluskov.lesson1;
+
+public class TypeJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello" + " " + "world");
+		byte x = 34, y = 13;
+		short w = 85, t = 475
+	    System.out.printin("t -=w :" -t);
+		
+		x += y;
+		System.out.println("x +=y :" + x);
+
+	}
+
+}
